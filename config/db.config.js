d16f72d985ad5a0b3bad0998eec047037b8de6f5
EmanuelMiron <emanuel_miron6@gmail.com>
2020-12-28 22:30:59 +0100

    mongoose setup + connection to database

---
+++ config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    HOST: 'localhost',
+    DB_PORT: '27017',
+    DB_NAME: 'BirthdayReminder'
+}
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server started on port:${PORT}` )
-})
+const dbConfig = require('./config/db.config');
+const {
+    HOST,
+    DB_PORT,
+    DB_NAME
+} = dbConfig;
+
+mongoose.connect(`mongodb://${HOST}:${DB_PORT}/${DB_NAME}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connected to the database!")
+        app.listen(PORT, () => {
+            console.log(`Server started on port:${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.error("Error", err);
+        process.exit();
+    })
