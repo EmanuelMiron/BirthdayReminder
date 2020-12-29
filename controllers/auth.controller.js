75fdb38f8f4ecaa5167445380b3573b1a364bf6d
EmanuelMiron <emanuel_miron6@gmail.com>
2020-12-29 20:31:25 +0100

    Created login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
+const config = require('../config/auth.config');
 const db = require('../models');
 const User = db.user;
 
 var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
 
 exports.register = (req, res) => {
@@ -26,4 +28,53 @@ exports.register = (req, res) => {
             message: "User has been registered successfully!"
         });
     })
+}
+
+exports.login = (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    User
+        .findOne({
+            email
+        })
+        .exec((err, user) => {
+            if (err) {
+                res.status(500).json({
+                    message: err
+                });
+                return;
+            }
+
+            if (!user) {
+                res.status(404).json({
+                    message: "User not found!"
+                });
+                return;
+            }
+
+            const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+            if (!passwordIsValid) {
+                res.status(401).json({
+                    message: "Incorrect Password",
+                    accessToken: null
+                })
+                return;
+            }
+
+            var token = jwt.sign({
+                id: user._id
+            }, config.secret, {
+                expiresIn: 86400 // 24 hours
+            })
+
+            res.status(200).json({
+                email: user.email,
+                accessToken: token
+            })
+
+        })
 }
