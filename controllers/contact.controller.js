f5dbea1d54c6815636628d89e4b965ac64632cc5
EmanuelMiron <emanuel_miron6@gmail.com>
2020-12-29 21:14:58 +0100

    Created Contact getAllContacts controller

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -11,12 +11,16 @@ exports.addContact = (req, res) => {
     const contact = new Contact(newContact)
 
     contact.save(err => {
-        if(err) {
-            res.status(500).json({message: err});
+        if (err) {
+            res.status(500).json({
+                message: err
+            });
             return;
         }
 
-        res.status(200).json({message: "Contact was created successfully!"})
+        res.status(200).json({
+            message: "Contact was created successfully!"
+        })
     })
 }
 
@@ -24,18 +28,39 @@ exports.getOneContact = (req, res) => {
     const contactId = req.params.id;
 
     Contact
-    .findOne({_id: contactId})
-    .exec((err, contact) => {
-        if(err){ 
-            res.status(500).json({message: err});
-            return;
-        }
+        .findOne({
+            _id: contactId
+        })
+        .exec((err, contact) => {
+            if (err) {
+                res.status(500).json({
+                    message: err
+                });
+                return;
+            }
 
-        if(!contact){
-            res.status(404).json({message: "Contact not found!"});
-            return;
-        }
+            if (!contact) {
+                res.status(404).json({
+                    message: "Contact not found!"
+                });
+                return;
+            }
 
-        res.status(200).json(contact)
-    })
+            res.status(200).json(contact)
+        })
+}
+
+exports.getAllContacts = (req, res) => {
+    Contact
+        .find()
+        .exec((err, contacts) => {
+            if (err) {
+                res.status(500).json({
+                    message: err
+                });
+                return;
+            }
+
+            res.status(200).json(contacts)
+        })
 }
