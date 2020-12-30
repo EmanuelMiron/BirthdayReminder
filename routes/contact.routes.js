fee3f8d1534ec94a67a3072449878162369bedda
EmanuelMiron <emanuel_miron6@gmail.com>
2020-12-30 22:39:33 +0100

    added verifyToken middleware to every Contact route

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,28 +1,34 @@
 const controllers = require('../controllers/contact.controller');
+const middlewares = require('../middlewares/verifyToken');
 
 module.exports = app => {
     app.post(
         "/api/contacts/addContact",
+        middlewares.verifyToken,
         controllers.addContact
     )
 
     app.get(
         "/api/contact/:id",
+        middlewares.verifyToken,
         controllers.getOneContact
     )
 
     app.get(
         "/api/contacts",
+        middlewares.verifyToken,
         controllers.getAllContacts
     )
 
     app.put(
         "/api/contact/:id",
+        middlewares.verifyToken,
         controllers.updateContact
     )
 
     app.delete(
         "/api/contact/:id",
+        middlewares.verifyToken,
         controllers.deleteContact
     )
 }
